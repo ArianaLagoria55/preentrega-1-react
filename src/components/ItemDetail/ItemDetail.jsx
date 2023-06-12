2bef941b1a72105283bfb100a9d4033171633d27
Tomas Gonzalez <tomasgonzalezn@hotmail.com>
2023-06-12 20:58:16 -0300

    "commit"

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,12 +1,12 @@
 import './ItemDetail.css'
 
-const ItemDetail = ({id, nombre, precio, img}) => {
+const ItemDetail = ({id, nombre, precio, descripcion,img}) => {
   return (
     <div className='contenedorItem'>
         <h2>Nombre: {nombre} </h2>
         <h3>Precio: {precio} </h3>
         <h3>ID: {id} </h3>
-        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga odio eveniet facere maiores quo tempore quisquam! Consectetur dolores quos ducimus maiores quam quae, eveniet voluptatibus beatae, nemo cumque tempore modi?</p>
+        <p> {descripcion}</p>
         <img src= {img} alt={nombre} />
     </div>
   )
